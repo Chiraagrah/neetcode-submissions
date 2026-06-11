4b1ab487a23f6021b9455cc9a4cf828e7efb6825
Chirag <147981564+Chiraagrah@users.noreply.github.com>
2026-06-11 14:25:24 -0400

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def che(self, se, x, start):
+        if x+1 not in se:
+            return start
+        else:
+            return self.che(se,x+1,start+1)
+    def longestConsecutive(self, nums: List[int]) -> int:
+        se = set(nums)
+        ma = 0
+        for x in nums:
+            if x-1 in se:
+                continue
+            else:
+                sta=1
+                temp=x+1
+                while temp in se:
+                    sta+=1
+                    temp+=1
+                if sta>ma:
+                    ma=sta
+        return ma
+
+    
