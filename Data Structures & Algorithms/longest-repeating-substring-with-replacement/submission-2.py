ceb1f29a40372740e48aa91410b7ee09f884ea13
Chirag <147981564+Chiraagrah@users.noreply.github.com>
2026-06-16 12:04:57 -0400

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        start = 0
+        end=0
+        ma = {}
+        max_cnt = 0
+        max_char=''
+        cn = 0
+        le = 0
+        while end<len(s):
+            if s[end] not in ma:
+                ma[s[end]]=1
+            else:
+                ma[s[end]]+=1
+            if ma[s[end]]>max_cnt:
+                max_cnt=ma[s[end]]
+                max_char=s[end]
+            if end-start+1-max_cnt>k:
+                ma[s[start]]-=1
+                if s[start]==max_char:
+                    max_cnt-=1
+                start+=1
+            le = max(le,end-start+1)
+            end+=1
+        return le
+        
