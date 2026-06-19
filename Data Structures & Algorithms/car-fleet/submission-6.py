104df24600ce8f110143ae3eceb6b6945e56f70e
Chirag <147981564+Chiraagrah@users.noreply.github.com>
2026-06-19 16:03:53 -0400

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.py
@@ -0,0 +1,10 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        temp = sorted(zip(position,speed),reverse=True)
+        stck = []
+        for x,y in temp:
+            y=(target-x)/y
+            if stck and stck[-1]>=y:
+                continue
+            stck.append(y)
+        return len(stck)
