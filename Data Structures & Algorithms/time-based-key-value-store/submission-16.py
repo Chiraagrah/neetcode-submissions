d9cb7c2c6d462ff1d9fde9024527802e0a766cf1
Chirag <147981564+Chiraagrah@users.noreply.github.com>
2026-06-20 20:34:59 -0400

    Add: time-based-key-value-store - submission-16

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-16.py
@@ -0,0 +1,28 @@
+class TimeMap:
+
+    def __init__(self):
+        self.dic={}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.dic:
+            self.dic[key] = [[timestamp,value]]
+        else:
+            self.dic[key].append([timestamp,value])
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.dic:
+            return ""
+        arr = self.dic[key]
+        start = 0
+        end = len(arr) - 1
+        ans=""
+        if timestamp < arr[0][0]:
+            return ""
+        while start<=end:
+            mid = (start + end) // 2
+            if timestamp < arr[mid][0]:
+                end = mid - 1
+            else:
+                ans = arr[mid][1]
+                start = mid + 1
+        return ans
