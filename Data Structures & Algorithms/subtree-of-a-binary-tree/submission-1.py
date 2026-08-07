3b139111e95da7ac79d347e6fca781e44df90cdf
Chirag <147981564+Chiraagrah@users.noreply.github.com>
2026-08-06 22:49:55 -0400

    Add: subtree-of-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-1.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def dfs(self,root, subroot):
+        if not root and not subroot:
+            return True
+        if (not root and subroot) or (not subroot and root) or (root.val!=subroot.val):
+            return False
+        return self.dfs(root.left,subroot.left) and self.dfs(root.right,subroot.right)   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root:
+            return False
+        if self.dfs(root,subRoot):
+            return True
+        return self.isSubtree(root.left,subRoot) or self.isSubtree(root.right,subRoot)
+            
